a080c1b3c112bd63a30b82a3adba3d86835cf365
Yeiber Cano <yeibercano@gmail.com>
2016-07-13 23:59:29 -0700

    fix typo in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import usersReducer from './users';
 
 const rootReducer = combineReducers({
-  users: userReducer
+  users: usersReducer
 });
 
 export default rootReducer;
