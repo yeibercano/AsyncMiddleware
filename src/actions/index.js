8c36f49f45a86c1d5705cc072230613c4c673184
Yeiber Cano <yeibercano@gmail.com>
2016-07-14 00:28:06 -0700

    remove hard code and add ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,11 @@
 import { FETCH_USERS } from './types';
+import axios from 'axios';
 
 export function fetchUsers() {
+	const request = axios.get('http://jsonplaceholder.typicode.com/users')
+	
 	return {
 		type: FETCH_USERS,
-		payload: [
-			{
-				name: 'John'
-			},
-			{
-				name: 'Carlos'
-			},
-			{
-				name: 'Juancito'
-			}
-		]
+		payload: request
 	}
 }
