859cb72b874596a77ecc277acd644956e48797bd
Yeiber Cano <yeibercano@gmail.com>
2016-07-13 23:37:44 -0700

    fix typo importing file and default

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,10 @@
-import { FETCH_USERS } from '../actions.types';
+import { FETCH_USERS } from '../actions/types';
 
 export default function(state = [], action) {
 	switch(action.type) {
 		case FETCH_USERS:
 			return [ ...state, ...action.payload];
 	}
-	default:
-	  return state;
+	
+	return state;
 }
